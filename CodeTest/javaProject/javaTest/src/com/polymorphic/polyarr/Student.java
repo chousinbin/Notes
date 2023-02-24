c6c392e63696c3a6ec78c0f4ff879e947f3bd92c
Sinbin <chousinbin@163.com>
2023-02-24 23:02:34 +0800

    20230224

---
+++ CodeTest/javaProject/javaTest/src/com/polymorphic/polyarr/Student.java
@@ -0,0 +1,34 @@
+package com.polymorphic.polyarr;
+
+/**
+ * @ClassName: Student
+ * @Description:
+ * @author: sinBin
+ * @date: 2023-02-23 11:21
+ * @version: 1.0
+ */
+public class Student extends Person{
+    private double score;
+
+    public Student(String name, int age, double score) {
+        super(name, age);
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String say() {
+        return super.say() + "\t" + score;
+    }
+
+    public void study() {
+        System.out.println("学生" + getName() + "正在上课");
+    }
+}
