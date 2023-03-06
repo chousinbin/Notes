fdab99891a2a59a296f62c76aa77ec4cca4f9789
Sinbin <chousinbin@163.com>
2023-03-06 21:50:45 +0800

    20230306

---
+++ CodeTest/javaProject/javaTest/src/com/object_/toString1.java
@@ -0,0 +1,18 @@
+package com.object_;
+
+/**
+ * @ClassName: toString1
+ * @Description:
+ * @date: 2023-03-06 17:18
+ */
+public class toString1 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+class monster {
+
+}
+
+
---
+++ CodeTest/javaProject/javaTest/src/com/polymorphic/static1.java
@@ -0,0 +1,33 @@
+package com.polymorphic;
+
+/**
+ * @ClassName: static1
+ * @Description:类对象
+ * @date: 2023-03-06 20:41
+ */
+public class static1 {
+    public static void main(String[] args) {
+        Person.nation = "China";
+        Person p1 = new Person("bin", 20);
+        Person p2 = new Person("jin", 18);
+
+        System.out.println(p1.nation);
+        System.out.println(p2.nation);
+
+        p1.nation = "CN";
+        System.out.println(p1.nation);
+        System.out.println(p2.nation);
+
+    }
+}
+
+class Person {
+    String name;
+    int age;
+    static String nation;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
