f73d719f1a923ea8ed60b7b1438e9b0bb41a664c
Sinbin <chousinbin@163.com>
2024-01-02 22:23:43 +0800

    2024-01-02

--- unsorted/gsp_drugstore/create table sale.sql
+++ unsorted/gsp_drugstore/create table sale.sql
@@ -1,13 +1,11 @@
 create table sale
 (
-	customer_id varchar(8),
-	drug_id varchar(8),
-
+	customer_id smallint,
+	drug_id smallint,
 	sale_quantity smallint not null,
-	sale_price decimal(9,2) check(sale_price > 0),
-	sale_sum_price decimal(9,2) check(sale_sum_price > 0),
-	sale_date date not null,
-
+	sale_price decimal(9,2) not null check(sale_price > 0),
+	sale_sum_price decimal(9,2) not null check(sale_sum_price > 0),
+	sale_datetime datetime not null,
 	constraint PK_customer_id_drug_id primary key (customer_id, drug_id),
 	constraint FK_sale_customer_id foreign key(customer_id) references customer(customer_id),
 	constraint FK_sale_drug_id foreign key(drug_id) references drug(drug_id)
--- unsorted/gsp_drugstore/create table stock.sql
+++ unsorted/gsp_drugstore/create table stock.sql
@@ -1,13 +1,13 @@
 create table stock
 (
-	drugstore_id varchar(8),
-	drug_id varchar(8),
-	stock_batch varchar(8) unique,
-	prodyction_date date not null,
-	stock_purchase_price decimal(9,2) check(stock_purchase_price > 0),
-	stock_sale_price decimal(9,2) check(stock_sale_price > 0),
+	drugstore_id smallint,
+	drug_id smallint,
+	stock_batch varchar(8) not null unique,
+	production_date date not null,
+	stock_purchase_price decimal(9,2) not null check(stock_purchase_price > 0),
+	stock_sale_price decimal(9,2) not null check(stock_sale_price > 0),
+	stock_quantity smallint not null,
 	stock_date date not null,
-	stock_quantity smallint not null
 	constraint PK_drugstore_id_drug_id primary key(drugstore_id, drug_id),
 	constraint FK_drugstore_id foreign key(drugstore_id) references drugstore(drugstore_id),
 	constraint FK_drug_id foreign key(drug_id) references drug(drug_id)
