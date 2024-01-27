6716c1f9b18ba1efe3530467dc06d55c44620ea9
Sinbin <chousinbin@163.com>
2024-01-28 00:54:03 +0800

    20240128

--- code/algorithm/exercise/00-00.cpp
+++ code/algorithm/exercise/00-00.cpp
@@ -1,47 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1010;
-int a[N][N], b[N][N];
-// 差分数组构造
-void insert(int x1, int y1, int x2, int y2, int c)
-{
-    b[x1][y1] += c;
-    b[x2 + 1][y1] -= c;
-    b[x1][y2 + 1] -= c;
-    b[x2 + 1][y2 + 1] += c;
-}
+const int N = 1e5 + 10;
+int a[N];
+int b[N];
 
 int main()
 {
-    int n, m, q;
-    cin >> n >> m >> q;
-    // 输入原数组，初始化差分数组
-    for(int i = 1; i <= n; i++)
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < m; i++) cin >> b[i];
+
+    /*
+        遍历主串，
+        当子指针与主指针数值相同时，子指针右移
+        遍历完主串，看子指针是否遍历完子串
+    */
+    int j = 0;
+    for(int i = 0; i < m; i++)
     {
-        for(int j = 1; j <= m; j++)
-        {
-            cin >> a[i][j];
-            insert(i, j, i, j, a[i][j]);
-        }
+        if(b[i] == a[j] && j < n) j++;
     }
-    // 差分数组区间加值
-    while(q--)
-    {
-        int x1, x2, y1, y2, c;
-        cin >> x1 >> y1 >> x2 >> y2 >>c;
-        insert(x1, y1, x2, y2, c);
-    }
-    // 最终结果是差分数组的前缀和
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= m; j++)
-        {
-            a[i][j] = a[i - 1][j] + a[i][j - 1] - 
-                a[i - 1][j - 1] + b[i][j];
-            cout << a[i][j] << ' ';
-        }
-        cout << endl;
-    }
+
+    if(j == n) cout << "Yes";
+    else cout << "No";
     return 0;
 }
