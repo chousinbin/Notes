4e5bb97fe44829a6937c0cb7f5c6e883d569329e
Sinbin <chousinbin@163.com>
2024-01-28 23:06:21 +0800

    20240128

--- code/algorithm/exercise/00-00.cpp
+++ code/algorithm/exercise/00-00.cpp
@@ -1,29 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 10;
-int a[N];
-int b[N];
-
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i = 0; i < m; i++) cin >> b[i];
-
-    /*
-        遍历主串，
-        当子指针与主指针数值相同时，子指针右移
-        遍历完主串，看子指针是否遍历完子串
-    */
-    int j = 0;
-    for(int i = 0; i < m; i++)
-    {
-        if(b[i] == a[j] && j < n) j++;
-    }
-
-    if(j == n) cout << "Yes";
-    else cout << "No";
-    return 0;
+	int q;
+	cin >> q;
+	
+	while(q--)
+	{
+		int n = 0;
+		
+		for(int i = 31; i >= 0; i--)
+		{
+			int x;
+			cin >> x;
+			n = (n << 1) + x;
+		}
+		
+		int a = n + 1, b = n + 3;
+		
+		for(int i = 31; i >= 0; i--)
+		{
+			cout << (a >> i & 1);
+		}
+		cout << endl;
+		for(int i = 31; i >= 0; i--)
+		{
+			cout << (b >> i & 1);
+		}
+		cout << endl;
+	}
+	return 0;
 }
