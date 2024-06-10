842c7111194cec435a335722a7fc3cf739adbe89
Sinbin <chousinbin@gmail.com>
2024-06-10 21:26:44 +0800

    20240610

--- code/CETMS/src/CETMS/Main.java
+++
@@ -1,9 +0,0 @@
-package CETMS;
-
-import CETMS.View.LoginView;
-
-public class Main {
-    public static void main(String[] args) {
-        new LoginView();
-    }
-}
--- code/CETMS/src/CETMS/View/StudentAddView.java
+++
@@ -1,4 +0,0 @@
-package CETMS.View;
-
-public class StudentAddView {
-}
