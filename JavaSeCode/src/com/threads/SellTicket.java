68936461b44338c0041066e78ca411babc7c0360
Sinbin <chousinbin@gmail.com>
2024-11-25 21:50:22 +0800

    Complete thread study

--- JavaSeCode/src/com/threads/SellTicket.java
+++ JavaSeCode/src/com/threads/SellTicket.java
@@ -11,30 +11,36 @@ package com.threads;
 public class SellTicket {
     public static void main(String[] args) {
 //        // fun 1
-//        Sell1 sell = new Sell1();
-//        Thread thread1 = new Thread(sell);
-//        Thread thread2 = new Thread(sell);
-//        Thread thread3 = new Thread(sell);
-//        thread1.start();
-//        thread2.start();
-//        thread3.start();
+        Sell1 sell = new Sell1();
+        Thread thread1 = new Thread(sell);
+        Thread thread2 = new Thread(sell);
+        Thread thread3 = new Thread(sell);
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
         // fun 2
-        new Sell2().start();
-        new Sell2().start();
-        new Sell2().start();
+//        new Sell2().start();
+//        new Sell2().start();
+//        new Sell2().start();
     }
 }
 
 class Sell1 implements Runnable {
     private static int ticketNum = 100;
     @Override
-    public synchronized void run() {
-        while (--ticketNum >= 0) {
-            System.out.println(Thread.currentThread().getName() + "售票" +
-                    "剩余" + ticketNum);
+    public void run() {
+        while (true) {
+            synchronized (this) {
+                if (ticketNum == 0) {
+                    break;
+                }
+                --ticketNum;
+                System.out.println(Thread.currentThread().getName() + "售票" +
+                        "剩余" + ticketNum);
+            }
             try {
-                Thread.sleep(50);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -45,19 +51,23 @@ class Sell1 implements Runnable {
 
 class Sell2 extends Thread {
     private static int ticketNum = 100;
+    // 锁在静态对象上，确保多个线程对象共享一个锁对象
     private static final Object lock = new Object(); // 锁对象
-    // 同步应该锁在临界资源上，run 方法是线程对象
     @Override
     public void run() {
-        synchronized (lock) {
-            while (--ticketNum >= 0) {
+        while (true) {
+            synchronized (lock) {
+                if (ticketNum == 0) {
+                    break;
+                }
+                --ticketNum;
                 System.out.println(Thread.currentThread().getName() + "售票" +
                         "剩余" + ticketNum);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
         System.out.println("已售完");
