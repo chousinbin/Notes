686ef7b48b5a967861495ec78f6415c38e00f5ba
Sinbin <chousinbin@gmail.com>
2025-01-16 01:00:32 +0800

    添加Java网络编程TCP部分代码

---
+++ JavaSeCode/src/com/net_/file_upload/TCPFileUploadClient.java
@@ -0,0 +1,40 @@
+package com.net_.file_upload;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * @Project: JavaSeCode
+ * @Author: SinbinZhou
+ * @Date: 2025/1/15 21:03
+ * @Description:
+ */
+public class TCPFileUploadClient {
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket(InetAddress.getLocalHost(), 8888);
+
+        // 读取文件到字节数组
+        BufferedInputStream bis = new BufferedInputStream(
+                new FileInputStream("resource\\client\\logo.jpg"));
+        byte[] bytes = StreamUtil.streamToByteArray(bis);
+
+
+        // 文件发送
+        BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+        bos.write(bytes);
+        socket.shutdownOutput();
+
+
+        // 获取信息
+        InputStream inputStream = socket.getInputStream();
+        String res = StreamUtil.streamToString(inputStream);
+        System.out.println(res);
+
+        inputStream.close();
+        bis.close();
+        bos.close();
+        socket.close();
+    }
+}
