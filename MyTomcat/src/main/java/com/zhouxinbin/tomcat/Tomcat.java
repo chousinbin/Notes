9e1f1aea4509265461679d9ae12ee5fdc346661b
sinpin <chousinbin@gmail.com>
2025-08-24 01:14:01 +0800

    MyTomcat 实现 Servlet 处理

--- MyTomcat/src/main/java/com/zhouxinbin/tomcat/Tomcat.java
+++ MyTomcat/src/main/java/com/zhouxinbin/tomcat/Tomcat.java
@@ -1,19 +1,60 @@
 package com.zhouxinbin.tomcat;
 
 import com.zhouxinbin.tomcat.handler.RequestHandler;
+import com.zhouxinbin.tomcat.servlet.MyHttpServlet;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Tomcat {
+    public static final ConcurrentHashMap<String, MyHttpServlet> servletMapping = new ConcurrentHashMap<>();
+    public static final ConcurrentHashMap<String, String> servletUrlMapping = new ConcurrentHashMap<>();
+    public static final String ApplicationConText = "/MyTomcat";
+
     public static void main(String[] args) throws IOException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.initHashMap();
+        tomcat.listening();
+    }
+    // dom4j 读取 web.xml 存储到哈希表，并自动创建 Servlet 实例，后期可以改为第一次请求时创建实例
+    public void initHashMap() throws IOException {
+        String path = Tomcat.class.getResource("/").getPath();
+        SAXReader saxReader = new SAXReader();
+        try {
+            Document document = saxReader.read(new File(path + "web.xml"));
+            Element root = document.getRootElement();
+            List<Element> elements = root.elements();
+            // 遍历过滤 Servlet Servlet-Mapping
+            for (Element element : elements) {
+                if (element.getName().equalsIgnoreCase("servlet")) {
+                    Element servletName = element.element("servlet-name");
+                    Element servletClass = element.element("servlet-class");
+                    servletMapping.put(servletName.getText(),
+                            (MyHttpServlet) Class.forName(servletClass.getText().trim()).newInstance());
+                } else if (element.getName().equalsIgnoreCase("servlet-mapping")) {
+                    Element servletName = element.element("servlet-name");
+                    Element urlPattern = element.element("url-pattern");
+                    servletUrlMapping.put(urlPattern.getText(), servletName.getText());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void listening() throws IOException {
         ServerSocket serverSocket = new ServerSocket(9090);
         System.out.println("Tomcat 在 9090 监听");
         while (!serverSocket.isClosed()) {
             // 等待客户端连接
             Socket socket = serverSocket.accept();
-            // 创建多线程
+            // 创建接收 HTTP 请求的多线程
             new Thread(new RequestHandler(socket)).start();
         }
     }
--- MyTomcat/src/main/java/com/zhouxinbin/tomcat/handler/RequestHandler.java
+++ MyTomcat/src/main/java/com/zhouxinbin/tomcat/handler/RequestHandler.java
@@ -1,8 +1,10 @@
 package com.zhouxinbin.tomcat.handler;
 
+import com.zhouxinbin.tomcat.Tomcat;
 import com.zhouxinbin.tomcat.http.MyRequest;
 import com.zhouxinbin.tomcat.http.MyResponse;
 import com.zhouxinbin.tomcat.servlet.MyCalServlet;
+import com.zhouxinbin.tomcat.servlet.MyHttpServlet;
 
 import java.io.*;
 import java.net.Socket;
@@ -23,8 +25,31 @@ public class RequestHandler implements Runnable{
             MyRequest myRequest = new MyRequest(socket.getInputStream());
             // 创建 HttpResponse 对象把响应返回到浏览器
             MyResponse myResponse = new MyResponse(socket.getOutputStream());
-            // 创建 Servlet
-            new MyCalServlet().doGet(myRequest, myResponse);
+            // 获取 URI 获得实例
+            String uri = myRequest.getUri();
+            // 判断应用上下文是否正确
+            String applicationContext = uri.substring(0, uri.lastIndexOf("/"));
+            if (!applicationContext.equals(Tomcat.ApplicationConText)) {
+                uri = "/";
+            }
+            // 判断是否存在实例
+            uri = uri.substring(uri.lastIndexOf("/"));
+            String servletName = Tomcat.servletUrlMapping.get(uri);
+            // 避免获取 URL 时 空指针异常
+            if (servletName == null) {
+                servletName = "";
+            }
+            MyHttpServlet myHttpServlet = Tomcat.servletMapping.get(servletName);
+            // 判断实例是否在 web.xml 中
+            if (myHttpServlet != null) {
+                // 动态绑定，调用运行类型的 GET 或 POST
+                myHttpServlet.service(myRequest, myResponse);
+            } else {
+                String responseBody = myResponse.responseHeader + "<h1> 404 NOT FOUND </h1>";
+                OutputStream outputStream = myResponse.getOutputStream();
+                outputStream.write(responseBody.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+            }
             // 关流
             inputStream.close();
             socket.close();
